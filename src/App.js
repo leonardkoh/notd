0223ae14eb1c1a60a290e3810048596aa4639126
LeonardK <40259905+leonardkoh@users.noreply.github.com>
2020-08-31 21:59:03 +1000

    style update

--- src/App.js
+++ src/App.js
@@ -9,11 +9,18 @@ let url = "https://www.abc.net.au/news/justin/";
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
       <Content />
       </header>
     </div>
   );
+}
+
+function Header() {
+  return (
+    <div>HEADER</div>
+  )
 }
 
 class Content extends React.Component {
@@ -36,12 +43,18 @@ class Content extends React.Component {
   render() {
     let arr = this.state.article_headings;
     const $ = cheerio.load(this.state.data);
+
     $('h4.heading').map((i,e) => { 
       arr.push($(e).text());
       console.log($(e).text()) })
+
     return(
+      this.state.article_headings.length === 0 ? <div>loading...</div> : 
       <div>
-        {this.state.article_headings}
+        <ul> { 
+          this.state.article_headings.map((e,i) => <li>{e}</li>
+        )}
+        </ul>
       </div>
     )
   }
